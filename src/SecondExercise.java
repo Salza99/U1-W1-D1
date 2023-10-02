a6009c580a4882a77de3b00b7e579c5386c5b988
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-02 15:11:50 +0200

    Second exercise done

--- src/SecondExercise.java
+++ src/SecondExercise.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class SecondExercise {
     public static void main(String[] args) {
 
-        System.out.println("ciao");
 
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci una stringa e premi invio");
+        String firstLine = input.nextLine();
+        System.out.println("Inserisci una seconda stringa e premi invio");
+        String secondLine = input.nextLine();
+        System.out.println("Inserisci un'ultima stringa e premi invio");
+        String thirdLine = input.nextLine();
+        System.out.println("stringhe in ordine: " + firstLine + " " + secondLine + " " + thirdLine);
+        System.out.println("stringhe in ordine inverso: " + thirdLine + " " + secondLine + " " + firstLine);
     }
+
+
 }
